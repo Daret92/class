4c7f2c404f5610ab043dd2d2ea16c4b50aa5b120
Daret92 <60407967+Daret92@users.noreply.github.com>
2021-01-02 09:22:00 -0600

    Added

--- users/views.py
+++ users/views.py
@@ -10,8 +10,15 @@ from decimal import Decimal
 @login_required(login_url='/accounts/login/')
 def index(request):
     obj = ProductInventory.objects.all()
+    carrito = carUser.objects.filter(user=request.user,is_pendient=True).first()
+    if carrito:
+        productos = productsCarUser.objects.filter(carUser=carrito)
+    else:
+        productos = None
     context={
-        "variable": obj
+        "variable": obj,
+        "carrito":carrito,
+        "productos":productos
     }
     return render(request,'index.html',context)
 
@@ -26,13 +33,12 @@ def addCar(request):
                 productoCarrito.save()
             else:
                 productoCarrito = productsCarUser.objects.create(carUser=carrito.first(),product=productoTmp,total=request.GET.get("total_product"))
-            return JsonResponse({'success':True,'mensaje':"Agregado al carrito"})
+            return JsonResponse({'success':True,'mensaje':"Agregado al carrito","pk_producto":productoCarrito.id,"name":productoCarrito.product.name,"total":productoCarrito.total})
         else:
             carrito = carUser.objects.create(user=request.user)
             productoCarrito = productsCarUser.objects.create(carUser=carrito,product=productoTmp,total=request.GET.get("total_product"))
-            return JsonResponse({'success':True,'mensaje':"Agregado al carrito"})
+            return JsonResponse({'success':True,'mensaje':"Agregado al carrito","pk_producto":productoCarrito.id,"name":productoCarrito.product.name,"total":productoCarrito.total})
     except Exception as e:
-        print(e)
         return JsonResponse({'success':False,'mensaje':"Ocurrio un error al integrar al carrito"})
 
 def logoutUser(request):
